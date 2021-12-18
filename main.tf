bcd875c3536b31f2b5bc91b520bcd84faed7b8b6
CobraTamer <tanyatamir@sky.com>
2021-12-18 18:52:31 +0000

    commit

--- main.tf
+++ main.tf
@@ -58,13 +58,6 @@ resource "azurerm_subnet" "SubnetGateway" {
   address_prefixes     = ["10.0.3.0/27"]
 }
 
-resource "azurerm_resource_group" "RGWE" {
-  name     = "WERG"
-  location = "West Europe"
-
-
-}
-
 # REGION WEST US
 
 
@@ -79,37 +72,76 @@ resource "azurerm_resource_group" "RGWUS" {
 
 resource "azurerm_virtual_network" "DevSerVNET" {
   name                = "DevServiceVNET"
-  resource_group_name = azurerm_resource_group.RGWE.name
-  location            = azurerm_resource_group.RGWE.location
+  resource_group_name = azurerm_resource_group.RGWUS.name
+  location            = azurerm_resource_group.RGWUS.location
   address_space       = ["10.10.0.0/16"]
   
 }
 
 
-resource "azurerm_subnet" "ServSubnet" {
-  name                 = "ServSubnet"
-  resource_group_name  = azurerm_resource_group.RGWE.name
-  virtual_network_name = azurerm_virtual_network.coreservVNET.name
+resource "azurerm_subnet" "AzuSubnet" {
+  name                 = "AzureSubnet"
+  resource_group_name  = azurerm_resource_group.RGWUS.name
+  virtual_network_name = azurerm_virtual_network.DevSerVNET.name
   address_prefixes     = ["10.10.1.0/24"]
 
 }
 
-resource "azurerm_subnet" "AppSubnet" {
-  name                 = "AppSubnet"
-  resource_group_name  = azurerm_resource_group.RGWE.name
-  virtual_network_name = azurerm_virtual_network.coreservVNET.name
+resource "azurerm_subnet" "AwsSubnet" {
+  name                 = "AwsSubnet"
+  resource_group_name  = azurerm_resource_group.RGWUS.name
+  virtual_network_name = azurerm_virtual_network.DevSerVNET.name
   address_prefixes     = ["10.10.2.0/27"]
 
 }
 
-resource "azurerm_subnet" "SubnetGateway" {
+resource "azurerm_subnet" "GcsSubnet" {
   name                 = "GatewaySubnet"
-  resource_group_name  = azurerm_resource_group.RGWE.name
-  virtual_network_name = azurerm_virtual_network.coreservVNET.name
+  resource_group_name  = azurerm_resource_group.RGWUS.name
+  virtual_network_name = azurerm_virtual_network.DevSerVNET.name
   address_prefixes     = ["10.10.3.0/27"]
 }
 
 
 
+#South Africa North
+resource "azurerm_resource_group" "RGSAN" {
+  name     = "SANRG"
+  location = "South Africa North"
 
+
+}
+
+
+resource "azurerm_virtual_network" "NetAutVNET" {
+  name                = "NetAutoVNET"
+  resource_group_name = azurerm_resource_group.RGSAN.name
+  location            = azurerm_resource_group.RGSAN.location
+  address_space       = ["10.20.0.0/16"]
+  
+}
+
+
+resource "azurerm_subnet" "SshSubnet" {
+  name                 = "SSHSubnet"
+  resource_group_name  = azurerm_resource_group.RGSAN.name
+  virtual_network_name = azurerm_virtual_network.NetAutVNET.name
+  address_prefixes     = ["10.20.1.0/24"]
+
+}
+
+resource "azurerm_subnet" "RESTSubnet" {
+  name                 = "RestAPISubnet"
+  resource_group_name  = azurerm_resource_group.RGSAN.name
+  virtual_network_name = azurerm_virtual_network.NetAutVNET.name
+  address_prefixes     = ["10.20.2.0/24"]
+
+}
+
+resource "azurerm_subnet" "NetconfSubnet" {
+  name                 = "NetConfAPISubnet"
+  resource_group_name  = azurerm_resource_group.RGSAN.name
+  virtual_network_name = azurerm_virtual_network.NetAutVNET.name
+  address_prefixes     = ["10.20.3.0/24"]
+  
 }
